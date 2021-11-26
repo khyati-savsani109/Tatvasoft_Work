cd0b82f14f4daec21524f8d6f831931aed33fc14
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2021-11-26 20:20:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,263 @@
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/lobs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <section class="sec1">
+    <div class="main">
+      <div class="logo"><img src="logo-large.png"></div>
+      <ul type="none">
+        <li><a href="">Book a Cleaner</a></li>
+        <li><a href="">Prices</a></li>
+        <li><a href="">Our Gaurantee</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact us</a></li>
+        <li><a href="">Login</a></li>
+        <li><a href="">Become a Helper</a></li>
+        <li><a href=""><img src="ic-flag.png"></a></li>
+        <li><a href=""><img src="sp-arrow-down.png"></a></li>
+      <ul>
+    </div>
+
+    <div class="title">
+      <p>Lorem ipsum text</p><br>
+        <div class="p1"><img src="ic-check.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscicing<br>
+                        <img src="ic-check.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscicing<br>
+                        <img src="ic-check.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscicing
+        </div>
+
+   <div class="main2">
+     <button align="center">Let's Book a Cleaner</button>
+
+<br><br><br>
+      <div>
+          <center>  <table border=0>
+        <tr align="center">
+          <td><img src="step-1.png"></td>
+          <td><img src="step-arrow-1.png"></td>
+          <td><img src="step-2.png"></td>
+          <td><img src="step-arrow-1-copy.png"></td>
+          <td><img src="step-3.png"></td>
+          <td><img src="step-arrow-1.png"></td>
+          <td><img src="step-4.png"></td>
+        </tr>
+
+       <tr></tr><tr></tr><tr></tr> <tr></tr><tr></tr><tr></tr>
+       <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+
+       <tr align="center">
+         <td class="tr1">Enter your postcode</td>
+         <td></td>
+         <td class="tr1">Select your plan</td>
+         <td></td>
+         <td class="tr1">Pay securely online</td>
+         <td></td>
+         <td class="tr1">Enjoy amazing service</td>
+       </tr>
+
+       <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+       <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+       <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+
+       <tr align="center">
+         <td></td>
+         <td></td>
+         <td></td>
+         <td><img src="group-18_5.png"></td>
+         <td></td>
+         <td></td>
+         <td></td>
+       </tr>
+     </table> </center>
+   </div></div>
+
+</div>
+
+</section>
+
+<section class="sec2">
+  <div class="main3">
+
+        <center><p>Why Helperland</p></center>
+
+        <div>
+          <center><table class="tab2" >
+            <tr align="center">
+              <td><img src="helper-img-1.png"></td>
+              <td><img src="group-23.png"></td>
+              <td><img src="group-24.png"></td>
+            </tr>
+
+            <tr align="center">
+              <td class="tr2">Experience & Vetted Professionals</td>
+              <td class="tr2">Secure Online Payment</td>
+              <td class="tr2">Dedicated Customer Service</td>
+            </tr>
+
+             <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+              <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+
+            <tr align="center">
+              <td class="tr3">dominate the industry in scale and scope with an adaptable, extensive network that consistently delivers exceptional results.</td>
+              <td class="tr3">Payment is processed securely online. Customers pay safely online and manage ghe booking.</td>
+              <td class="tr3">to our customers and are guided in all we do by their needs. The team is always happy to support you and offer all the information.</td>
+            </tr>
+          </table></center>
+        </div>
+
+  </div>
+</section>
+
+<section class="sec3">
+
+
+    <div id="divleft"><br><br><br>
+      <p class="w1">Lorem ipsum dolor amet, consectetur</p><br>
+      <p class="w2"> Lorem ipsum dolor sit amet, consectuter adipiscicing elit. donec nisi sapien, suscipit ut accumsan vitae, pulvinar ac libero.</p><br>
+      <p class="w2">Aliquam erat voltpat. Nullman quis ex odio. Nam bibendum cursus purus, vel efficitur urna finibus vitae. Nullma finibus aliquat pharetra. Morbi in sem odor, Integer pretium hendrerit ante quis vehicula.</p><br>
+      <p class="w2">Mauris consequat ornare enim, sed lobortis quam ultrices sed.</p>
+    </div>
+
+    <div id="divright">
+      <img src="group-36.png">
+    </div>
+
+
+
+<div class="main4">
+      <center><p>Our Blog</p></center>
+
+        <center><table class="tab4">
+          <tr>
+            <td><img src="group-28.png"></td>
+            <td><img src="group-29.png"></td>
+            <td><img src="group-30.png"></td>
+          </tr>
+
+          <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr>
+
+          <tr>
+            <td class="y4">Lorem ipsum dolor sit amet</td>
+            <td class="y4">Lorem ipsum dolor sit amet</td>
+            <td class="y4">Lorem ipsum dolor sit amet</td>
+          </tr>
+
+          <tr>
+            <td class="y1">January 28, 2019</td>
+            <td class="y1">January 28, 2019</td>
+            <td class="y1">January 28, 2019</td>
+          </tr>
+
+          <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+
+          <tr>
+            <td class="y2">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus<br> pulvinar aliquet.</td>
+            <td class="y2">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus <br>pulvinar aliquet.</td>
+            <td class="y2">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus <br>pulvinar aliquet.</td>
+          </tr>
+
+          <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+
+          <tr>
+            <td class="y3">Read the post&nbsp;<img src="shape-2.png"></td>
+            <td class="y3">Read the post&nbsp;<img src="shape-2.png"></td>
+            <td class="y3">Read the post&nbsp;<img src="shape-2.png"></td>
+          </tr>
+
+      </table></center>
+</div>
+
+
+
+</section>
+
+<br>
+
+<section class="sec4">
+      <h2>What Our Customers Say</h2><br><br><br><br><br><br>
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="group-31.png" style="width:150px; height:auto;paddin-top:10px">
+      <h1>Lary Whatson</h1>
+      <p class="y1">Manchester</p><br>
+        <p class="d1">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus pulvinar aliquet consequat. Praesent nec, malesuada nibh.</p><br>
+        <p class="d1">Nullman et metus congue, auctor augue sit amet,consectetur totor.</p><br><br>
+        <p class="d1">Read More&nbsp;<img src="shape-2.png" style="width:40px"></p>
+      </div>
+    </div>
+
+    <div class="column">
+      <div class="card">
+        <img src="group-32.png" style="width:150px; height:auto;paddin-top:10px">
+        <h1>John Smith</h1>
+        <p class="y1">Manchester</p><br>
+          <p class="d1">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus pulvinar aliquet consequat. Praesent nec, malesuada nibh.</p><br>
+          <p class="d1">Nullman et metus congue, auctor augue sit amet,consectetur totor.</p><br><br>
+          <p class="d1">Read More&nbsp;<img src="shape-2.png" style="width:40px"></p>
+        </div>
+      </div>
+
+
+      <div class="column">
+        <div class="card">
+          <img src="group-33.png" style="width:150px; height:auto;paddin-top:10px">
+          <h1>Lars Jhonson</h1>
+          <p class="y1">Manchester</p><br>
+            <p class="d1">Lorem ipsum dolor sit amet, consectuter adipiscicing elit. Sed fermentum metus pulvinar aliquet consequat. Praesent nec, malesuada nibh.</p><br>
+            <p class="d1">Nullman et metus congue, auctor augue sit amet,consectetur totor.</p><br><br>
+            <p class="d1">Read More&nbsp;<img src="shape-2.png" style="width:40px"></p>
+          </div>
+        </div>
+
+  </div>
+<br><br>
+<div class="cont1">
+
+<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="preview.png" width="50px" height="50px">
+
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+<font size=5 face="arial" >GET OUR NEWSLETTER</font>
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+  <img src="layer-598.png" width="50px" height="50px"></p>
+  <br>
+
+  <a href="" class="b1">YOUR EMAIL</a>&nbsp;&nbsp;<a href="" class="b2">Submit</a><br><br><br><br>
+
+
+<div class="c1">
+    <div class="log1"><img src="logo-small.png"></div>
+    <center><ul type="none" class="nv">
+      <li><a href="">HOME</a></li>
+      <li><a href="">ABOUT</a></li>
+      <li><a href="">TESTIMONIALS</a></li>
+      <li><a href="">FAQS</a></li>
+      <li><a href="">INSURANCE POLICY</a></li>
+      <li><a href="">IMPRESSUM</a></li>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+      <li><a href=""><img src="ic-facebook.png"></a></li>
+      <li><a href=""><img src="ic-instagram.png"></a></li>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    </ul>
+  </div>
+</div>
+</div>
+
+</section>
+</body>
+</html>
