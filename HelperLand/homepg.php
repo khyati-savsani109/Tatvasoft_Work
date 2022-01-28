2c628c61d5c40aa9821e6ef274d9976e1e53b762
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-01-28 20:22:47 +0530

    Add files via upload

---
+++ HelperLand/homepg.php
@@ -0,0 +1,266 @@
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+  		<div class="container conta1" style="background-image: url(assets/images/sec1-img.png);">
+			<header class="header">
+				<div class="container cont_6">
+					<div class="row align-items-center justify-content-between">
+						<div class="logo"><a href=""><img src="assets/images/logo-large.png"></a></div>
+						<input type="checkbox" id="nav-check">
+						<label for="nav-check" class="nav-toggler">
+						<span></span>
+						</label>
+						<nav class="nav">
+						<ul>
+							<li><a href="">Book a Cleaner</a></li>
+							<li><a href="http://localhost/HelperLand/View/prices.php">Prices</a></li>
+							<li><a href="">Our guarantee</a></li>
+							<li><a href="">Blog</a></li>
+							<li><a href="http://localhost/HelperLand/View/contact-us.php">Contact us</a></li>
+							<li><a data-target="#mymodel" data-toggle="modal" href="">Login</a></li>
+							<li><a href="">Become a helper</a></li>
+							<li class="dropdown"><a href="" class="dropdown-toggle" data-toggle="dropdown"><img class="img1" src="assets/images/ic-flag.png">
+                        <div class="dropdown-menu"></div></a></li>
+						</ul>
+						</nav>
+					</div>
+				</div>
+			</header>
+
+
+				<div class="row">
+					<div class="col">
+						<h1>Lorem ipsum text</h1>
+						<p class="pa1"><img src="assets/images/ic-check.png"> Lorem ipsum dolor sit amet,consectetur adipiscing<br>
+									   <img src="assets/images/ic-check.png"> Lorem ipsum dolor sit amet,consectetur adipiscing<br>
+									   <img src="assets/images/ic-check.png"> Lorem ipsum dolor sit amet,consectetur adipiscing</p>
+					</div>
+				</div>
+				<br><br>
+				<center><button class="b1">Let's Book a Cleaner</button></center>
+
+				<div class="container c1">
+					<div class="row row_1">
+						<div class="col-md-2"><center><img src="assets/images/step-1.png"><br><p class="pa2">Enter your postcode</p></center></div>
+						<div class="col-md-1 mt-2"><center><img src="assets/images/step-arrow-1.png"></center></div>
+						<div class="col-md-2"><center><img src="assets/images/step-2.png"><br><p class="pa2">Select your plan</p></center></div>
+						<div class="col-md-1 mt-2"><center><img src="assets/images/step-arrow-1-copy.png"></center></div>
+						<div class="col-md-2"><center><img src="assets/images/step-3.png"><br><p class="pa2">Pay securely online</p></center></div>
+						<div class="col-md-1 mt-2"><center><img src="assets/images/step-arrow-1.png"></center></div>
+						<div class="col-md-2"><center><img src="assets/images/step-4.png"><br><p class="pa2">Enjoy amazing service</p></center></div>
+					</div>
+				</div>
+
+				<center><img class="img2" src="assets/images/group-18_5.png"></center>
+				<br>
+		</div>
+
+		<div class="container con1 py-4">
+			<h3>Why Helperland</h3>
+			<br><br>
+				<div class="row row_2">
+				<div class="col-md-3 offset-md-1"><center><img src="assets/images/helper-img-1.png" class="pic2"><p class="pa6"><br>Experienced & Vetted Professionals</p>
+												  <p class="pa7">dominate the industry in scale and scope with an adaptable, extensive network that consistently delivers exceptional results.</p></center></div>
+
+				<div class="col-md-3"><center><img src="assets/images/group-23.png" class="pic2"><p class="pa6"><br>Secure Online Payment</p><br>
+												  <p class="pa7">Payment is processed securely online. Customers pay safely online and manage the booking.</p></center></div>
+
+				<div class="col-md-3"><center><img src="assets/images/group-24.png" class="pic2"><p class="pa6"><br>Dedicated Customer Service</p>
+												  <p class="pa7">to our customers and are guided in all we do by their needs. The team is always happy to support you and offer all the information.</p></center></div>
+				</div>
+		</div>
+
+		<br><br><br>
+
+		<div class="container con1">
+				<img src="assets/images/blog-left-bg.png" class="div-left"><img src="assets/images/blog-right-bg.png" class="div-right">
+
+				<div class="row">
+						<div class="col-md-12 tab2">
+							<div class="table-responsive">
+								<table class="table table-borderless">
+									<tbody>
+										<tr>
+											<td><p class="pa3">Lorem ipsum dolor sit amet, consectetur</p>
+											  <p class="pa4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nisi sapien, suscipit ut accumsan vitae, pulvinar ac libero.<br><br>
+															 Aliquam erat volutpat. Nuliam quis ex odio. Nam bibendum cursus purus, vel efficitur urna finibus vitae. Nullam finibus aliquet pharetra. Morbi in sem dolor. Integer pretium hendreit ante quis vehicula.<br><br>
+															 Mauris consequat ornare enim, sed lobortis quam ultrices sed.</p></td>
+
+											<td></td><td></td><td></td><td></td><td></td><td></td>
+
+											<td><img class="pic4" src="assets/images/group-36.png"></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+
+					<br>
+
+					<h3>Our Blog</h3>
+					<div class="row">
+						<div class="col-md  tab1">
+							<div class="table-responsive">
+								<table class="table table-borderless">
+									<tbody>
+										<tr>
+											<td><img class="pic5" src="assets/images/group-28.png">
+												<p class="pa8">Lorem ipsum dolor sit amet</p>
+												<p class="pa9">January 28,2019</p>
+												<p class="pa10">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet.<br><br>
+												Read the Post <img src="assets/images/shape-2.png"></p></td>
+											<td></td>
+											<td><img class="pic5" src="assets/images/group-29.png">
+												<p class="pa8">Lorem ipsum dolor sit amet</p>
+												<p class="pa9">January 28,2019</p>
+												<p class="pa10">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet.<br><br>
+												Read the Post <img src="assets/images/shape-2.png"></p></td>
+											<td></td>
+											<td><img class="pic5" src="assets/images/group-30.png">
+												<p class="pa8">Lorem ipsum dolor sit amet</p>
+												<p class="pa9">January 28,2019</p>
+												<p class="pa10">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet.<br><br>
+												Read the Post <img src="assets/images/shape-2.png"></p></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+		</div>
+
+<br><br><br><br>
+		<div class="container con2">
+
+
+			<div class="wrapper">
+			<h3>What Our Customers Say</h3>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body">
+								<img src="assets/images/group-31.png"><br><br>
+									<h5>Lary Watson</h5><p class="l1">Manchester</p>
+									<p class="l2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet consequat.Praesant nec malesuada nibh.<br><br>
+									Nullam et metus congue,auctor augue sit amet,consectetur tortor.<br><br>Read More <img src="assets/images/shape-2.png"></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body">
+								<img src="assets/images/group-32.png"><br><br>
+									<h5>John Smith</h5><p class="l1">Manchester</p>
+									<p class="l2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet consequat.Praesant nec malesuada nibh.<br><br>
+									Nullam et metus congue,auctor augue sit amet,consectetur tortor.<br><br>Read More <img src="assets/images/shape-2.png"></p>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-6 col-lg-4">
+							<div class="card">
+								<div class="card-body">
+								<img src="assets/images/group-33.png"><br><br>
+									<h5>Lars Johnson</h5><p class="l1">Manchester</p>
+									<p class="l2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed fermentum metus pulvinar aliquet consequat.Praesant nec malesuada nibh.<br><br>
+									Nullam et metus congue,auctor augue sit amet,consectetur tortor.<br><br>Read More <img src="assets/images/shape-2.png"></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<br><br>
+
+
+				<div class="row">
+					<div class="col-md-1"><img src="assets/images/preview.png" class="pic3"></div>
+					<div class="col-md-5 offset-md-2 col_1"><h6>GET OUR NEWSLETTER</h6>
+					<center><button class="d1">&nbsp;&nbsp;YOUR EMAIL</button>&nbsp;
+					<button class="d2">Submit</button></center></div>
+					<div class="col-md-1 offset-md-3"><img src="assets/images/layer-598.png" class="pic3"></div>
+				</div><br><br>
+			</div>
+
+		<div class="container cont_3">
+				<div class="row">
+					<div class="col-md-1 offset-md-1"><img class="logo_1" src="assets/images/footer-logo.png"></div>
+
+					<div class="col-md-6  offset-md-1">
+						<ul>
+						  <li><a href="http://localhost/HelperLand/homepg.php">HOME</a></li>
+						  <li><a href="http://localhost/helperland/View/about.php">ABOUT</a></li>
+						  <li><a href="">TESTIMONIALS</a></li>
+						  <li><a href="http://localhost/HelperLand/View/faqs.php">FAQS</a></li>
+						  <li><a href="">INSURANCE POLICY</a></li>
+						  <li><a href="">IMPRESSUM</a></li>
+						</ul>
+					</div>
+
+					<div class="col-md-1  offset-md-1"><img class="logo_2" src="assets/images/ic-facebook.png"><img class="logo_2" src="assets/images/ic-instagram.png"></div>
+				</div>
+
+					<center><hr color="#3d3c3a" width="1250px"></center>
+
+			<p class="copyright">&copy;2018 Helperland All rights reserved. Terms and Conditions &nbsp; | &nbsp; Privacy Policy</p>
+
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="modal fade" id="mymodel">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h3 class="text-center text-gray">Login to your account</h3>
+						<button type="button" class="close" data-dismiss="modal"> &times; </button>
+					</div>
+
+					<div class="modal-body">
+						<form>
+							<div class="form-group">
+								<input type="text" placeholder="E-mail address" class="form-control">
+								<br>
+								<input type="password" placeholder="Password" class="form-control">
+								<br>
+								<input type="checkbox"> Remember me
+								<br>
+								<br>
+								<center><input type="login" value="Login" class="subbut" data-dismiss="modal"></center>
+								<br>
+								<center><font color="#009999">Forgot password?</font>
+								<br>
+								Don't have an account? <a href="http://localhost/HelperLand/View/create-account.php"><font color="#009999">Create an account</font></a></center>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+		<div class="modal fade" id="mymodel-1">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h3 class="text-center text-gray">Register Now!</h3>
+						<button type="button" class="close" data-dismiss="modal"> &times; </button>
+					</div>
+</body>
+
+</html>
