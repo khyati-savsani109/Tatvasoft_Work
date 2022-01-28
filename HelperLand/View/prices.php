b748ecf9f3acc6aa664614087ee68bf75df1be25
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-01-28 20:44:15 +0530

    Add files via upload

---
+++ HelperLand/View/prices.php
@@ -0,0 +1,181 @@
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="../assets/css/prices_style.css">
+</head>
+
+<body>
+
+		<div class="container conta1">
+		<header class="header">
+			<div class="container cont_6">
+				<div class="row align-items-center justify-content-between">
+					<div class="logo"><a href=""><img src="../assets/images/logo-large.png"></a></div>
+					<input type="checkbox" id="nav-check">
+					<label for="nav-check" class="nav-toggler">
+					<span></span>
+					</label>
+					<nav class="nav">
+					<ul>
+						<li><a class="active" href="">Book now</a></li>
+						<li><a class="active1" href="">Prices & services</a></li>
+						<li><a class="active2" href="">Warranty</a></li>
+						<li><a class="active2" href="">Blog</a></li>
+						<li><a class="active2" href="http://localhost/HelperLand/View/contact-us.php">Contact</a></li>
+						<li><a class="active" href="">Login</a></li>
+						<li><a class="active3" href="">Become a helper</a></li>
+					</ul>
+					</nav>
+				</div>
+			</div>
+			</header>
+			<img src="../assets/images/group-18.png" class="img-fluid">
+
+
+
+		<div class="container"><br>
+			<h4>Prices</h4>
+
+			<center><img src="../assets/images/forma-1-copy-5.png"></center>
+			<br>
+
+			<center><div class="table-responsive">
+				<table class="table table-bordered">
+					<tr class="one">
+						<td>One Time</td>
+					</tr>
+
+					<tr class="one_1">
+						<td><p class="pa1"><font size=6>&euro; <b>20</b></font>/hr</p>
+						<p class="pa2"><img src="../assets/images/forma-1_5.png"> Fixed and Fair price<br>
+										<img src="../assets/images/forma-1_5.png"> Simple & secure online payment<br>
+										<img src="../assets/images/forma-1_5.png"> Manage and change orders online</p>
+						</td>
+					</tr>
+				</table>
+			</div>
+
+			<hr>
+			</center>
+
+			<h4>Extra services</h4>
+			<center><img src="../assets/images/forma-1-copy-5.png"></center>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-2 offset-md-1"><center><p class="pa3"><img src="../assets/images/3.png" class="pic"><br><p class="pa4">Inside cabinets</p>
+													  <p class="pa5">30 minutes</p></center></div>
+
+					<div class="col-md-2"><center><p class="pa3"><img src="../assets/images/5.png" class="pic"><br><p class="pa4">Inside fridge</p>
+													  <p class="pa5">30 minutes</p></center></div>
+
+					<div class="col-md-2"><center><p class="pa3"><img src="../assets/images/4.png" class="pic"><br><p class="pa4">Inside oven</p>
+													  <p class="pa5">30 minutes</p></center></div>
+
+					<div class="col-md-2"><center><p class="pa3"><img src="../assets/images/2.png" class="pic"><br><p class="pa4">Laundry wash & dry</p>
+													  <p class="pa5">30 minutes</p></center></div>
+
+					<div class="col-md-2"><center><p class="pa3"><img src="../assets/images/1.png" class="pic"><br><p class="pa4">Interior windows</p>
+													  <p class="pa5">30 minutes</p></center></div>
+				</div>
+			</div>
+			<br><br>
+		</div>
+
+		<div class="container cont_4">
+		<br><br>
+			<h4>What we include in cleaning</h4>
+			<center><img src="../assets/images/forma-1-copy-5.png "></center>
+			<br>
+
+				<div class="row row_1">
+				<div class="col-md-3 offset-md-1"><center><img src="../assets/images/group-18_3.png" class="pic2"><p class="pa6"><br>Living Room & Bedrooms</p>
+												  <p class="pa7"><img src="../assets/images/ic-check.png"> Dust removal on all accessible surfaces and electronic devices<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning of mirrors, glass and other surfaces<br>
+																 <img src="../assets/images/ic-check.png"> Clean the floor<br>
+																 <img src="../assets/images/ic-check.png"> Empty of wastebaskets<br>
+															 	 <img src="../assets/images/ic-check.png"> Clean up<br>
+																 <img src="../assets/images/ic-check.png"> Make beds</p></center></div>
+
+				<div class="col-md-3"><center><img src="../assets/images/group-18_4.png" class="pic2"><p class="pa6"><br>Bathrooms</p>
+												  <p class="pa7"><img src="../assets/images/ic-check.png"> Cleaning of sink,toilet,shower and tub<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning of mirrors, glass and other surfaces<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning of mirrors, glass and other surfaces<br>
+																 <img src="../assets/images/ic-check.png"> Clean the floor<br>
+																 <img src="../assets/images/ic-check.png"> Empty of waste containers</p></center></div>
+
+				<div class="col-md-3"><center><img src="../assets/images/group-18_2.png" class="pic2"><p class="pa6"><br>Kitchen</p>
+												  <p class="pa7"><img src="../assets/images/ic-check.png"> Cleaning the sink<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning the stove(outside)<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning of work surfaces<br>
+																 <img src="../assets/images/ic-check.png"> Dust removal on accessible surfaces<br>
+																 <img src="../assets/images/ic-check.png"> Cleaning the floors<br>
+															   <img src="../assets/images/ic-check.png"> Emptying of waste containers</p></center></div>
+				</div>
+				<br><br>
+			</div>
+
+			<div class="container">
+			<br><br>
+			<h4>Why Helperland</h4>
+			<center><img src="../assets/images/forma-1-copy-5.png"></center>
+
+			<div class="row">
+				<div class="col-md-4"><p class="dleft"><p class="pa8">Experienced and vetted professionals</p>
+							   <p class="pa9">dominate the industry in scale and scope with<br>an adaotable, extensive network that<br>consistently delivers exceptional results.</p>
+								<br>
+								<p class="pa8">Dedicated customer service</p>
+							   <p class="pa9">to our customers and are guided in all we do<br>by their needs. The team is always happy to<br>support you and offer all the information.<br>you need.</p></p>
+							</div>
+
+				<div class="col-md-1"><img class="dmid" src="../assets/images/the-best-img-1.png"></div>
+
+				<div class="col-md-4 offset-md-3"><p class="dright"><p class="pa10">Every cleaning is insured</p>
+							   <p class="pa11">and seek to provide exceptional service and<br>engage in proactive behavior. We'd be happy<br>to clean your homes.</p>
+								<br>
+								<p class="pa10">Secure online payment</p>
+							   <p class="pa11">Payment is processed securely online.<br>Customers pay safely online and manage<br>the booking.</p></p>
+
+							</div>
+			</div>
+			</div>
+			<br>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-md-1"><img src="../assets/images/preview.png" class="pic3"></div>
+					<div class="col-md-5 offset-md-2 col_1"><h6>GET OUR NEWSLETTER</h6>
+					<center><button class="d1">&nbsp;&nbsp;YOUR EMAIL</button>&nbsp;
+					<button class="d2">Submit</button></center></div>
+					<div class="col-md-1 offset-md-3"><img src="../assets/images/layer-598.png" class="pic3"></div>
+				</div><br><br>
+			</div>
+
+			<div class="container cont_3">
+				<div class="row">
+				<div class="col-md-1 offset-md-1"><img class="logo_1" src="../assets/images/footer-logo.png"></div>
+
+				<div class="col-md-6  offset-md-1">
+					<ul>
+					  <li><a href="http://localhost/HelperLand/homepg.php">HOME</a></li>
+					  <li><a href="http://localhost/helperland/View/about.php">ABOUT</a></li>
+					  <li><a href="">TESTIMONIALS</a></li>
+					  <li><a href="http://localhost/HelperLand/View/faqs.php">FAQS</a></li>
+					  <li><a href="">INSURANCE POLICY</a></li>
+					  <li><a href="">IMPRESSUM</a></li>
+					</ul>
+				</div>
+
+				<div class="col-md-1  offset-md-1"><img class="logo_2" src="../assets/images/ic-facebook.png"><img class="logo_2" src="../assets/images/ic-instagram.png"></div>
+				</div>
+
+				<center><hr color="#3d3c3a" width="1250px"></center>
+
+			<p class="copyright">&copy;2018 Helperland All rights reserved. Terms and Conditions &nbsp; | &nbsp; Privacy Policy</p>
+			</div>
+		</div>
+</body>
+
+</html>
