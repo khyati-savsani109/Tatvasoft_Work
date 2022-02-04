9021f2824788c100c0bddb4428170832c38c60ab
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-02-04 15:35:52 +0530

    Update create-account.php

--- HelperLand/View/create-account.php
+++ HelperLand/View/create-account.php
@@ -1,9 +1,32 @@
+<?php
+
+session_start();
+
+include( "../model/db_connection.php");
+
+if(isset($_POST['register']));{
+  @$firstname = $_POST['fname'];
+  @$lastname = $_POST['lname'];
+  @$email = $_POST['email'];
+  @$password = $_POST['pass'];
+  @$mobileno = $_POST['number'];
+}
+
+$qry = "INSERT INTO user(Firstname,Lastname,Email,Password,Mobile) VALUES('$firstname','$lastname','$email','$password','$mobileno')";
+
+$result = mysqli_query($conn,$qry);
+
+if(!$result){
+  echo "data not inserted";
+}
+
+ ?>
 <html>
 
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" href="../assets/css/create-account.css">
+  <link rel="stylesheet" href="../assets/css/create-account.css">
 </head>
 
 <body>
@@ -17,56 +40,60 @@
 					<div class="col-lg-2"></div>
 
 					<div class="col-lg-8">
-						<form class="form-group">
+						<form class="form-group" method="post" name="myform" autocomplete="off" onsubmit="return validation()">
 
 							<div class="row py-2">
 								<div class="col-lg-6">
-									<input type="text" name="fname" placeholder="First name" class="form-control">
+									<input type="text" name="fname" id="fname" placeholder="First name" class="form-control" required>
+                  <span id="msg1" style="color:red;"></span>
 								</div>
 
 								<div class="col-lg-6">
-									<input type="text" name="lname" placeholder="Last name" class="form-control">
+									<input type="text" name="lname" id="lname" placeholder="Last name" class="form-control" required>
+                  <span id="msg2" style="color:red;"></span>
 								</div>
 							</div>
 
 							<div class="row py-2">
 								<div class="col-lg-6">
-									<input type="email" name="email" placeholder="E-mail address" class="form-control">
+									<input type="email" name="email" value="" placeholder="E-mail address" class="form-control" required>
+                  <span id="msg3" style="color:red;"></span>
 								</div>
 
 								<div class="col-lg-6">
 									<div class="input-group-prepend">
 										<div class="input-group-text">+49</div>
-									<input type="text" name="number" placeholder="Mobile number" class="form-control">
+									<input type="text" name="number" id="mobile" placeholder="Mobile number" class="form-control" required>
 									</div>
+                  <span id="msg" style="color:red;"></span>
 								</div>
 							</div>
 
 							<div class="row py-2">
 								<div class="col-lg-6">
-									<input type="password" name="pass" placeholder="Password" class="form-control">
+									<input type="password" name="pass" placeholder="Password" class="form-control" required>
 								</div>
 
 								<div class="col-lg-6">
-									<input type="password" name="cpass" placeholder="Confirm Password" class="form-control">
+									<input type="password" name="cpass" placeholder="Confirm Password" class="form-control" required>
 								</div>
 							</div>
 
 							<div class="row py-3">
 								<div class="col-lg-6">
-									<input type="checkbox" name="checkbox"> I have read the <font color="#00ccff">privacy policy.</font>
+									<input type="checkbox" name="checkbox" required> I have read the <font color="#00ccff">privacy policy.</font>
 								</div>
 							</div>
 
 							<div class="row py-2">
 								<div class="col-lg-12">
-									<center><input type="register" name="register" value="Register" class="subbut"></center>
+									<center><input type="submit" name="register" value="Register" class="subbut"></center>
 								</div>
 							</div>
 
 							<div class="row py-2">
 								<div class="col-lg-12">
-									<center>Already registered? <a class="p1" href="">Login now</a></font></center>
+									<center>Already registered? <a class="p1"  href="http://localhost/HelperLand/homepg.php">Login now</a></font></center>
 								</div>
 							</div>
 
@@ -76,6 +103,8 @@
 
 					<div class="col-lg-2"></div>
 			</div>
+
+      <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
