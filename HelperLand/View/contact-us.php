3389a06a6b696fba80617f560af58be91271613e
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-02-04 15:35:14 +0530

    Update contact-us.php

--- HelperLand/View/contact-us.php
+++ HelperLand/View/contact-us.php
@@ -1,14 +1,16 @@
 <?php
 
-include( "../model/contact-us-db.php");
+session_start();
 
-if(isset($_POST['submit'])){
-  $firstname = $_POST['fname'];
-  $lastname = $_POST['lname'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $mobileno = $_POST['number'];
-  $message = $_POST['message'];
+include( "../model/db_connection.php");
+
+if(isset($_POST['submit']));{
+  @$firstname = $_POST['fname'];
+  @$lastname = $_POST['lname'];
+  @$email = $_POST['email'];
+  @$subject = $_POST['subject'];
+  @$mobileno = $_POST['number'];
+  @$message = $_POST['message'];
 }
 
 $qry = "INSERT INTO contactus(Firstname,Lastname,Email,Subject,PhoneNumber,Message) VALUES('$firstname','$lastname','$email','$subject','$mobileno','$message')";
@@ -26,7 +28,7 @@ if(!$result){
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<link rel="stylesheet" href="../assets/css/contact_style.css">
+  <link rel="stylesheet" href="../assets/css/contact_style.css">
 </head>
 
 <body>
@@ -80,15 +82,17 @@ if(!$result){
 					<div class="col-lg-3"></div>
 
 					<div class="col-lg-6">
-						<form class="form-group" method="POST">
+						<form class="form-group" method="post" name="myform" autocomplete="off" onsubmit="return validation()">
 
 							<div class="row">
 								<div class="col-lg-6">
-									<input type="text" name="fname" placeholder="First name" class="form-control mb-4">
+									<input type="text" name="fname" id="fname" placeholder="First name" class="form-control mb-4" required>
+                  <span id="msg1" style="color:red;"></span>
 								</div>
 
 								<div class="col-lg-6">
-									<input type="text" name="lname" placeholder="Last name" class="form-control">
+									<input type="text" name="lname" id="lname" placeholder="Last name" class="form-control" required>
+                  <span id="msg2" style="color:red;"></span>
 								</div>
 							</div>
 
@@ -96,17 +100,20 @@ if(!$result){
 								<div class="col-lg-6">
 									<div class="input-group-prepend">
 										<div class="input-group-text mb-4">+49</div>
-									<input type="text" name="number" placeholder="Mobile number" class="form-control mb-4">
+									<input type="text" name="number" id="mobile" placeholder="Mobile number" class="form-control mb-4" required>
 									</div>
+                  <span id="msg" style="color:red;"></span>
 								</div>
+
 								<div class="col-lg-6">
-									<input type="email" name="email" placeholder="Email address" class="form-control">
+									<input type="email" name="email" value="" placeholder="Email address" class="form-control" required>
+                  <span id="msg3" style="color:red;"></span>
 								</div>
 							</div>
 
 							<div class="row">
 								<div class="col-lg-12">
-									<select class="form-control" name="subject">
+									<select class="form-control" name="subject" required>
                     <option>Subject</option>
 										<option>General</option>
                     <option>Inquiry</option>
@@ -118,7 +125,7 @@ if(!$result){
 
 							<div class="row">
 								<div class="col-lg-12">
-									<textarea class="form-control" placeholder="Message" rows="6" name="message"></textarea>
+									<textarea class="form-control" placeholder="Message" rows="6" name="message" required></textarea>
 								</div>
 							</div>
 
@@ -168,6 +175,8 @@ if(!$result){
 			<p class="copyright">&copy;2018 Helperland All rights reserved. Terms and Conditions &nbsp; | &nbsp; Privacy Policy</p>
 			</div>
 		</div>
+
+<script src="../assets/js/script.js"></script>
 
 </body>
 
