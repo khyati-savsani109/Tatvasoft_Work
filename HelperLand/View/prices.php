b068670d0ff8e8dd20f10cb0189dc60883e08805
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-02-19 19:35:33 +0530

    Update prices.php

--- HelperLand/View/prices.php
+++ HelperLand/View/prices.php
@@ -19,7 +19,7 @@
 					</label>
 					<nav class="nav">
 					<ul>
-						<li><a class="active" href="">Book now</a></li>
+						<li><a class="active" href="http://localhost/helperland/View/book-now.php">Book now</a></li>
 						<li><a class="active1" href="">Prices & services</a></li>
 						<li><a class="active2" href="">Warranty</a></li>
 						<li><a class="active2" href="">Blog</a></li>
@@ -163,8 +163,8 @@
 					  <li><a href="http://localhost/helperland/View/about.php">ABOUT</a></li>
 					  <li><a href="">TESTIMONIALS</a></li>
 					  <li><a href="http://localhost/HelperLand/View/faqs.php">FAQS</a></li>
-					  <li><a href="">INSURANCE POLICY</a></li>
-					  <li><a href="">IMPRESSUM</a></li>
+						<li><a href="http://localhost/helperland/view/insurance-policy.php">INSURANCE COVERAGE</a></li>
+						<li><a href="http://localhost/helperland/view/impressum.php">IMPRESSUM</a></li>
 					</ul>
 				</div>
 
@@ -177,4 +177,5 @@
 			</div>
 		</div>
 </body>
+
 </html>
