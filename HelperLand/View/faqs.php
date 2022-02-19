3d0f418fe66a16410a0784f0d18a9ca73f761746
Savsani Khyati <87753065+khyati-savsani109@users.noreply.github.com>
2022-02-19 19:34:53 +0530

    Update faqs.php

--- HelperLand/View/faqs.php
+++ HelperLand/View/faqs.php
@@ -3,6 +3,8 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="../assets/css/faqs_style.css">
 </head>
 
@@ -19,7 +21,7 @@
 					</label>
 					<nav class="nav">
 					<ul>
-						<li><a class="active" href="">Book now</a></li>
+						<li><a class="active" href="http://localhost/helperland/View/book-now.php">Book now</a></li>
 						<li><a class="active1" href="http://localhost/HelperLand/View/prices.php">Prices & services</a></li>
 						<li><a class="active2" href="">Warranty</a></li>
 						<li><a class="active2" href="">Blog</a></li>
@@ -34,47 +36,107 @@
 
 			<img src="../assets/images/faq-banner.png" class="img-fluid" >
 
-			<div class="container cont_1"><br>
+			<div class="container cont_1 mt-5"><br>
 				<h2>FAQS</h2>
 				<center><img src="../assets/images/forma-1-copy-5.png"></center><br>
 
 				<p class="para-1">Whether you are Customer or Service Provider,<br>
 			   We have tried our best to solve all your queries and questions.</p>
 
-				<center><button class="b1">FOR CUSTOMER</button><button class="b2">FOR SERVICE PROVIDER</button></center>
-			</div>
 
-			<div class="container cont_1"><br>
-			<p class="para-2"><img src="../assets/images/down-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
-			<center><p class="para-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id diam tincidunt, fringilla ante vitae, dapibus velit. Vivamus id
-tortor rhoncus, effcitur quam at, suscipit tortor. Integer fermentum convallis eros vel semper. Ut non imperdiet velit. Praesent
-eu dui vel lacus porta eleifend eget quis dui. Integer tempus massa in gravida tincidunt. Fusce in libero tristique, euismod nisi
-vel, luctus urna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec et placerat arcu. Suspendisse lacinia
-tristique massa. Etiam risus justo, scelerisque id arcu eu, sodales tempor eros. Aliquam efficitur pretium urna, sit amet congue
-risus malesuada rutrum. Donec id massa vel velit ullamcorper accumsan ut eget nisi. Fusce viverra commodo lacus, sit amet
-facilisis leo luctus dictum.</p></center>
+				 <div class="container">
+					 <div class="row justify-content-center mt-5">
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+						 <div class="accordion d-md-flex" id="accorex">
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+							 <div class="card">
+									 	<button class="btn text-center" data-toggle="collapse" data-target="#custque">FOR CUSTOMER</button>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+							 		<div id="custque" class="collapse show" data-parent="#accorex">
+								 	<div class="card-body text-left">
+									 <a href="#link1" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> What is included in the basic cleaning?</a><br>
+									 <div id="link1" class="collapse">
+										 <div class="text2">Living room, bedroom and bathroom, kitchen<br> and common areas</div>
+									 </div>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+									 <a href="#link2" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> Which helper comes to me?</a><br>
+									 <div id="link2" class="collapse">
+										 <div class="text2">Heperland verfugt uber ein ausgedehntes<br>
+											 									Network an erfahrenen Reinungskraften. Je<br>
+																				nach Ort, Datum und Dauer versuchen wir dir<br>
+																				den besten verfugbaren Helper zuzuweisen.<br>
+																				Nimmit eine Reinungskraft deinen Auftrag an,<br>
+																				erhalst du eine E-mail mit dem Profil des<br>
+																				Helpers. Wenn Du unseren Service ofter nutzt,<br>
+																				kannst du eine Reinungskraft als Favroit<br>
+																				speichern. Bei kungftigen Buchungen wird<br>
+																				zunachst dein Favorit angefragt.</div>
+									 </div>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+									 <a href="#link3" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> Can I skip or move bookings?</a><br>
+									 <div id="link3" class="collapse">
+										 <div class="text2">48 Stunden vor einem Einstatz kannst du eine<br>
+										 										Buchung kostenlos andern. Mochtest du eine<br>
+																				bereits gebuchten Einstatz uberspringen,<br>
+																				schreiben wir den Wert der Buchung deinem<br>
+																				Konto gut. Das Guthaben kannst du fur eine<br>
+																				kungftige Buchung verwenden.</div>
+								 	 </div>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+									 <a href="#link4" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> Do I have to be at home during the cleaning?</a>
+									 <div id="link4" class="collapse">
+										 <div class="text2">We recommend that you be at home for the<br>
+										 										first cleaning. This way you get to know your<br>
+																				helper personally. You can also show him the<br>
+																				rooms and respond to your wishes. Of course<br>
+																				you can arrange an appointment or place for<br>
+																				the key handover with your helper. With<br>
+																				frequent bookings, some customers choose to<br>
+																				leave a spare key to their cleaner.</div>
+									 </div>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+									</div>
+						  		</div>
+					  </div>
 
-			<p class="para-2"><img src="../assets/images/right-arrow.png">&nbsp;&nbsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nisi nunc, iaculis mattis tellus ac ut non imperdiet velit?</p>
+					  <div class="card">
+								<button class="btn text-center collapsed" data-toggle="collapse" data-target="#helpque">FOR HELPERS</button>
 
-			</div>
+					  	<div id="helpque" class="collapse" data-parent="#accorex">
+						 	<div class="card-body text-left">
+							 <a href="#link5" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> How can I register?</a><br>
+							 <div id="link5" class="collapse">
+								 <div class="text2">Test answer</div>
+							 </div>
 
-			<br>
+							 <a href="#link6" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> How much do service providers earn?</a><br>
+							 <div id="link6" class="collapse">
+								 <div class="text2">The freelancers who work with Helperland set<br>
+								 										their own payouts, meaning they decide how<br>
+																		much they make per hour.</div>
+							 </div>
 
-			<div class="container con1">
+							 <a href="#link7" data-toggle="collapse" class="text1"><img src="../assets/images/right-arrow.png"> What support do you offer the service providers?</a><br>
+							 <div id="link7" class="collapse">
+								 <div class="text2">Our call center is available to service providers<br>
+								 										with any questions or problems regarding their bookings<br>
+																		during office hours, Before a service provider receives<br>
+																		orders, each individual patner receives an organisation session to<br>
+																		familiarise themselves with online platform and its profile.</div>
+							 </div>
+
+							</div>
+				    	</div>
+			     </div>
+
+		    </div>
+
+	    </div>
+	  </div>
+
+		</div>
+
+			<div class="container cont1 mt-4">
 			<h6>GET OUR NEWSLETTER</h6>
 			<center><button class="d1">&nbsp;&nbsp;YOUR EMAIL</button>&nbsp;
 			<button class="d2">Submit</button></center>
@@ -91,8 +153,8 @@ facilisis leo luctus dictum.</p></center>
 					  <li><a href="http://localhost/helperland/View/about.php">ABOUT</a></li>
 					  <li><a href="">TESTIMONIALS</a></li>
 					  <li><a href="http://localhost/HelperLand/View/faqs.php">FAQS</a></li>
-					  <li><a href="">INSURANCE POLICY</a></li>
-					  <li><a href="">IMPRESSUM</a></li>
+						<li><a href="http://localhost/helperland/view/insurance-policy.php">INSURANCE COVERAGE</a></li>
+						<li><a href="http://localhost/helperland/view/impressum.php">IMPRESSUM</a></li>
 					</ul>
 				</div>
 
@@ -103,7 +165,7 @@ facilisis leo luctus dictum.</p></center>
 
 			<p class="copyright">&copy;2018 Helperland All rights reserved. Terms and Conditions &nbsp; | &nbsp; Privacy Policy</p>
 			</div>
-		</div>
+	 	</div>
 
 </body>
 
